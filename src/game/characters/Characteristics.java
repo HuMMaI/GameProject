770cc6f1d42937615499ebde7f5be41dad9f1743
hummai <shining.melon2015@gmail.com>
2020-06-27 14:47:45 +0300

    Added fields to characteristics

--- src/game/characters/Characteristics.java
+++ src/game/characters/Characteristics.java
@@ -1,5 +1,50 @@
 package game.characters;
 
 public class Characteristics {
+	private int strength;
+	
+	private int agility;
+	
+	private int intelligence;
+	
+	private int defense;
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getAgility() {
+		return agility;
+	}
+
+	public void setAgility(int agility) {
+		this.agility = agility;
+	}
+
+	public int getIntelligence() {
+		return intelligence;
+	}
+
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+
+	public int getDefense() {
+		return defense;
+	}
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+	@Override
+	public String toString() {
+		return "Characteristics [strength=" + strength + ", agility=" + agility + ", intelligence=" + intelligence
+				+ ", defense=" + defense + "]";
+	}
 
 }
