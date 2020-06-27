b145fb55bd72f24bb10b46813227cc8bb72633fa
unknown <cherina97@gmail.com>
2020-06-27 15:30:27 +0300

    add City and EnemyTerritory classes

--- src/game/locations/City.java
+++ src/game/locations/City.java
@@ -1,5 +1,26 @@
 package game.locations;
 
+import game.characters.Blacksmith;
+import game.characters.Dealer;
+
 public class City {
+	
+	private Dealer dealer;
+	private Blacksmith blacksmith;
+	
+	public Dealer getDealer() {
+		return dealer;
+	}
+	public void setDealer(Dealer dealer) {
+		this.dealer = dealer;
+	}
+	public Blacksmith getBlacksmith() {
+		return blacksmith;
+	}
+	public void setBlacksmith(Blacksmith blacksmith) {
+		this.blacksmith = blacksmith;
+	}
+	
+	
 
 }
--- src/game/locations/EnemyTerritory.java
+++ src/game/locations/EnemyTerritory.java
@@ -1,5 +1,30 @@
 package game.locations;
 
+import java.util.List;
+
+import game.characters.Monster;
+
 public class EnemyTerritory {
+	
+	private String name;	
+	private List <Monster> monster;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Monster> getMonster() {
+		return monster;
+	}
+	public void setMonster(List<Monster> monster) {
+		this.monster = monster;
+	}
+	
+	
+	
+	
+	
 
 }
