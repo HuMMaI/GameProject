02308ce9da117f296338e8c1e9bf4bea95e1ebfc
hummai <shining.melon2015@gmail.com>
2020-06-27 15:37:41 +0300

    Added fields to dungeon

--- src/game/locations/Dungeon.java
+++ src/game/locations/Dungeon.java
@@ -1,5 +1,42 @@
 package game.locations;
 
+import java.util.List;
+import java.util.Map;
+
+import game.characters.Monster;
+
 public class Dungeon {
 
+	private String name;
+	
+	private int numOfStages;
+	
+	private Map<Integer, List<Monster>> monsters;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumOfStages() {
+		return numOfStages;
+	}
+
+	public void setNumOfStages(int numOfStages) {
+		this.numOfStages = numOfStages;
+	}
+
+	public Map<Integer, List<Monster>> getMonsters() {
+		return monsters;
+	}
+
+	public void setMonsters(Map<Integer, List<Monster>> monsters) {
+		this.monsters = monsters;
+	}
+	
+	
+	
 }
